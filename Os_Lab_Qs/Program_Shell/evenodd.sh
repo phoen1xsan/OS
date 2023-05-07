15a804cef479ab7894a22c09dbc833866575b6bf
Akash-Ingle <96162799+Akash-Ingle@users.noreply.github.com>
2023-05-07 11:19:03 +0530

    Delete Os_Lab_Qs/Program_Shell directory

--- Os_Lab_Qs/Program_Shell/evenodd.sh
+++
@@ -1,8 +0,0 @@
-echo "Enter a number"
-read n
-if [ `expr $n % 2` == 0 ]
-then
-echo "$n is a even number"
-else
-echo "$n is a odd number"
-fi
--- Os_Lab_Qs/Program_Shell/prime_range.sh
+++
@@ -1,9 +0,0 @@
-echo -n "Enter Two numbers"
-read a
-read b
-if [ a > b ]
-then
-echo "$a is bigger than $b"
-else
-echo "$b is bigger than $a"
-fi
